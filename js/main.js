0e61ad7aeff32fb22599dcce4e4e8d7b1c8f10ee
Claire Tregunna <c.tregunna1980@gmail.com>
2021-01-28 16:07:41 +0000

    initial commit, overlay and basic home page structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const overlay = document.querySelector('.welcome');
+const welcomeText = document.querySelector('.welcome_text');
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        overlay.classList.add('hide');
+        welcomeText.classList.add('hidden');
+        // alert('Page is loaded');
+	}, 6000);
+});
